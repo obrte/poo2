c284d9917360d9f9c0396ec4958321238581927e
Omar Bracamonte <omar.bracamonte@gmail.com>
2018-07-07 14:42:52 -0700

    llenado del DataGridView

--- poo2/Form1.cs
+++ poo2/Form1.cs
@@ -12,7 +12,9 @@ namespace poo2
 {
     public partial class Form1 : Form
     {
+
         private List<Productos> listaProductos = new List<Productos>();
+        Productos[] producto = new Productos[3];
         public Form1()
         {
             InitializeComponent();
@@ -26,10 +28,26 @@ namespace poo2
             //producto.pPrecio = 36.00f;
             //rtb1.Text += producto.pCodigo + Environment.NewLine + producto.pNombre + Environment.NewLine + producto.pPrecio;
             //Comentario nuevo
+            producto[0] = new Productos(1, "producto 1", 35.00f);
+            producto[1] = new Productos(2, "producto 2", 35.02f);
+            producto[2] = new Productos(3, "producto 3", 35.08f);
+            cargaProducto();
+
         }
         private void cargaProducto()
         {
+            try
+            {
+                for (int i = 0; i < producto.Length; i++)
+                {
+                    dgv1.Rows.Add(producto[i].pCodigo, producto[i].pNombre, producto[i].pPrecio);
+                }
+            }
+            catch (Exception err)
+            {
 
+                MessageBox.Show("ERROR:" + Environment.NewLine + err);
+            }
         }
     }
 
@@ -40,6 +58,15 @@ namespace poo2
         public String pNombre = "";
         public float pPrecio = 0.00f;
 
-
+        public Productos()
+        {
+            MessageBox.Show("Me llamaron sin parametros");
+        }
+        public Productos(long pCodigo, String pNombre, float pPrecio)
+        {
+            this.pCodigo = pCodigo;
+            this.pNombre = pNombre;
+            this.pPrecio = pPrecio;
+        }
     }
 }
